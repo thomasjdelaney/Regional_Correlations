e0aa8c264881cce4581136a6daab7ecfc258bbd4
Thomas Delaney <thomas.delaney@posteo.net>
2019-03-28 17:50:55 +0000

    Added plot closing line.

--- py/cross_correlograms.py
+++ py/cross_correlograms.py
@@ -16,7 +16,7 @@ from scipy.stats import pearsonr
 parser = argparse.ArgumentParser(description='For making cross-correlogram plots. ')
 parser.add_argument('-g', '--group', help='The quality of sorting for randomly chosen_cells.', default='good', choices=['good', 'mua', 'unsorted'], type=str, nargs='*')
 parser.add_argument('-r', '--region', help='The region for which we want to make the cross correlograms', type=str, default='thalamus', choices=['motor_cortex', 'striatum', 'hippocampus', 'thalamus', 'v1'])
-parser.add_argument('-i', '--stim_id', help='The id of the stimulus condition to use.')
+parser.add_argument('-i', '--stim_id', help='The id of the stimulus condition to use.', default=2, type=int)
 parser.add_argument('-s', '--numpy_seed', help='The seed to use to initialise numpy.random.', default=1798, type=int)
 parser.add_argument('-d', '--debug', help='Enter debug mode.', default=False, action='store_true')
 args = parser.parse_args()
@@ -86,6 +86,7 @@ def plotWithStdErrors(measure, std_errors, delays, ylabel, pair, region, stim_id
     filename = 'cross_correlogram_' + region + '_' + str(pair[0]) + '_' + str(pair[1]) + '_' + str(stim_id) + '.png'
     filepath = os.path.join(image_dir, 'cross_correlograms', filename)
     plt.savefig(filepath)
+    plt.close()
     return filepath
 
 def main():
@@ -103,9 +104,9 @@ def main():
     exp_frame = rc.getExperimentFrame(responding_cells, trials_info, spike_time_dict, cell_info, 0.001)
     delays = np.arange(-1000, 1001)
     for pair in responding_pairs:
-        print(dt.datetime.now().isoformat() + ' INFO: ' + 'Processing' + str(pair) + '...')
+        print(dt.datetime.now().isoformat() + ' INFO: ' + 'Processing ' + str(pair) + '...')
         correlations, corr_std_errors = getCrossCorrelogramByPair(pair, exp_frame, num_trials, delays)
-        filepath = plotWithStdErrors(correlations, corr_std_errors, delays, "Correlation Coefficient (a.u.)", pair, region, stim_id)
+        filepath = plotWithStdErrors(correlations, corr_std_errors, delays, "Correlation Coefficient (a.u.)", pair, args.region, args.stim_id)
         print(dt.datetime.now().isoformat() + ' INFO: ' + 'Saved:' + filepath)
     print(dt.datetime.now().isoformat() + ' INFO: ' + 'Done.')
 
