40766afe8c203224a687b11d05af09d21f135ab5
Thomas James Delaney <thomas.delaney@posteo.net>
2019-04-02 00:52:57 +0100

    Added bifurcating spike correlation figures.

--- py/bin_width_plotting.py
+++ py/bin_width_plotting.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
 parser = argparse.ArgumentParser(description="For plotting the data found in 'all_regions_stims_pairs_widths.csv'.")
-parser.add_argument('-c', '--correlation_type', help='Signal correlation or spike count correlation.', default='spike_count', choices=['spike_count', 'signal'], type=str)
+parser.add_argument('-c', '--correlation_type', help='Signal correlation or spike count correlation.', default='spike_count', choices=['spike_count', 'signal', 'bifurcation'], type=str)
 parser.add_argument('-f', '--filename', help='The file from which to get the pairwise correlation data.', type=str, default='all_regions_stims_pairs_widths.csv')
 parser.add_argument('-p', '--image_file_prefix', help='A prefix for the image file names.', type=str, default='')
 parser.add_argument('-x', '--x_axis_scale', help='The scale of the x-axis', type=str, default='linear', choices=['log', 'linear'])
@@ -62,6 +62,47 @@ def plotAbsSignalCorrByBinWidth(correlation_frame, region, region_to_colour, pre
     region_frame.loc[:,'corr_coef'] = region_frame.loc[:,'corr_coef'].abs()
     plotCorrCoefByBinWidth(region_frame, region, 0, region_to_colour, prefix, x_axis_scale)
 
+def getPositiveNegativePairs(region_stim_frame):
+    unique_pairs = np.unique(region_stim_frame[['first_cell_id', 'second_cell_id']].values, axis=0)
+    negative_pairs = np.array([])
+    positive_pairs = np.array([])
+    for pair in unique_pairs:
+        pair_frame = region_stim_frame.loc[(region_stim_frame.first_cell_id == pair[0])&(region_stim_frame.second_cell_id == pair[1])]
+        pair_mean = pair_frame['corr_coef'].mean()
+        if pair_mean < 0:
+            negative_pairs = np.hstack([negative_pairs, pair])
+        else:
+            positive_pairs = np.hstack([positive_pairs, pair])
+    return positive_pairs.reshape([positive_pairs.size//2, 2]).astype(int), negative_pairs.reshape([negative_pairs.size//2, 2]).astype(int)
+
+def plotPositiveOrNegative(pd_frame, region, colour, x_axis_scale, frame_label):
+    agg_frame = pd_frame[['bin_width','corr_coef']].groupby('bin_width').agg({'corr_coef':['mean', 'std']})
+    agg_frame.loc[:,'std_err'] = agg_frame.corr_coef.loc[:,'std']/np.sqrt(agg_frame.shape[0])
+    plt.plot(agg_frame.corr_coef.index.values, agg_frame.corr_coef['mean'], color=colour, label=frame_label)
+    plt.fill_between(agg_frame.corr_coef.index.values, agg_frame.corr_coef['mean']-agg_frame['std_err'], agg_frame.corr_coef['mean']+agg_frame['std_err'], color=colour, alpha=0.3)
+
+def plotBifurcatedCorrelationsByBinWidth(correlation_frame, region, stim_id, prefix, x_axis_scale):
+    region_stim_frame = correlation_frame[(correlation_frame.region == region)&(correlation_frame.stim_id == stim_id)]
+    positive_pairs, negative_pairs = getPositiveNegativePairs(region_stim_frame)
+    positive_frame = pd.concat([region_stim_frame.loc[(region_stim_frame.first_cell_id == p[0])&(region_stim_frame.second_cell_id == p[1])]for p in positive_pairs], ignore_index=True)
+    negative_frame = pd.concat([region_stim_frame.loc[(region_stim_frame.first_cell_id == p[0])&(region_stim_frame.second_cell_id == p[1])]for p in negative_pairs], ignore_index=True)
+    fig = plt.figure(figsize=(4,3))
+    plotPositiveOrNegative(positive_frame, region, 'blue', x_axis_scale, 'Correlated')
+    plotPositiveOrNegative(negative_frame, region, 'orange', x_axis_scale, 'Anti-correlated')
+    plt.plot([0, rc.bin_widths.max()], [0,0], color='black', linestyle='dashed')
+    plt.xlim([rc.bin_widths.min(), rc.bin_widths.max()])
+    plt.xscale('log') if x_axis_scale=='log' else 0
+    plt.ylim([-1,1])
+    plt.xlabel('Bin width (s)', fontsize='large')
+    plt.ylabel('Corr Coef (a.u.)', fontsize='large')
+    plt.legend(fontsize='large', loc='lower left')
+    plt.title(region.replace('_', ' ').capitalize(), fontsize='large')
+    plt.tight_layout()
+    filename = prefix + 'bin_width_relative_correlations_' + region + '_' + str(stim_id) + '.png'
+    print(dt.datetime.now().isoformat() + ' INFO: ' + 'Saving '+ filename + '...')
+    plt.savefig(os.path.join(image_dir, 'correlations_vs_bin_width', 'relative_correlations', x_axis_scale, filename))
+    plt.close()
+
 def main():
     print(dt.datetime.now().isoformat() + ' INFO: ' + 'Starting main function...')
     print(dt.datetime.now().isoformat() + ' INFO: ' + 'Loading all_regions_stims_pairs_widths.csv...')
@@ -74,6 +115,9 @@ def main():
             plotAbsCorrCoefByBinWidthForRegionStim(correlation_frame, region, best_stim, region_to_colour, args.image_file_prefix, args.x_axis_scale)
         elif args.correlation_type == 'signal':
             plotAbsSignalCorrByBinWidth(correlation_frame, region, region_to_colour, args.image_file_prefix, args.x_axis_scale)
+        elif args.correlation_type == 'bifurcation':
+            best_stim = getBestStimFromRegion(correlation_frame, region)
+            plotBifurcatedCorrelationsByBinWidth(correlation_frame, region, best_stim, args.image_file_prefix, args.x_axis_scale)
         else:
             sys.exit(dt.datetime.now().isoformat() + ' ERROR: ' + 'Unrecognised correlation type.')
 
