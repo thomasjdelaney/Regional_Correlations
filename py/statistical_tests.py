66bbb43186349a90f7c1b77352d56e1e701f000d
Thomas James Delaney <thomas.delaney@posteo.net>
2019-04-08 23:41:35 +0100

    Added stats testing script.

--- py/statistical_tests.py
+++ py/statistical_tests.py
@@ -9,7 +9,7 @@ import pandas as pd
 import datetime as dt
 from itertools import combinations
 from scipy.io import loadmat
-from scipy.stats import mannwhitneyu
+from scipy.stats import ks_2samp
 
 parser = argparse.ArgumentParser(description='For carrying out statistical tests on histograms of data.')
 parser.add_argument('-f', '--filename', help='The file in which to find the correlations.', type=str, default='all_regions_stims_pairs_widths.csv')
@@ -31,25 +31,19 @@ image_dir = os.path.join(proj_dir, 'images')
 sys.path.append(py_dir)
 import regionalCorrelations as rc
 
-def getRegionToHistDict(correlation_frame, bin_width, measure, max_mi):
-    region_to_hist = {}
-    if measure == 'corr_coef':
-        hist_bins = np.linspace(-1,1,25)
-    elif measure == 'mutual_info':
-        hist_bins = np.linspace(0,max_mi,25)
-    else:
-        sys.exit(dt.datetime.now().isoformat() + ' ERROR: ' + 'Unrecognised measure!')
+def getRegionToSampleDict(correlation_frame, bin_width, measure):
+    region_to_sample = {}
     for region in rc.regions:
         best_stim = rc.getBestStimFromRegion(correlation_frame, region)
         region_bin_frame = correlation_frame[(correlation_frame.region == region)&(correlation_frame.bin_width == bin_width)&(correlation_frame.stim_id == best_stim)]
         print(dt.datetime.now().isoformat() + ' INFO: ' + 'Mean ' + measure + ' = ' + str(region_bin_frame[measure].mean()))
         print(dt.datetime.now().isoformat() + ' INFO: ' + 'Median ' + measure + ' = ' + str(region_bin_frame[measure].median()))
-        region_to_hist[region] = np.histogram(region_bin_frame[measure], bins=hist_bins)[0]
-    return region_to_hist
+        region_to_sample[region] = region_bin_frame.loc[:,measure]
+    return region_to_sample
 
-def getKSStatPVal(regions, region_to_corr_hist, region_to_info_hist):
-    corr_stat, corr_p_val = mannwhitneyu(region_to_corr_hist[regions[0]], region_to_corr_hist[regions[1]])
-    info_stat, info_p_val = mannwhitneyu(region_to_info_hist[regions[0]], region_to_info_hist[regions[1]])
+def getKSStatPVal(regions, region_to_corr, region_to_info):
+    corr_stat, corr_p_val = ks_2samp(region_to_corr[regions[0]], region_to_corr[regions[1]])
+    info_stat, info_p_val = ks_2samp(region_to_info[regions[0]], region_to_info[regions[1]])
     return regions[0], regions[1], corr_stat, corr_p_val, info_stat, info_p_val
 
 def main():
@@ -59,15 +53,16 @@ def main():
     stim_info = loadmat(os.path.join(mat_dir, 'experiment2stimInfo.mat'))
     correlation_frame = pd.read_csv(os.path.join(csv_dir, args.filename))
     max_mi = correlation_frame.mutual_info.max()
-    region_to_corr_hist = getRegionToHistDict(correlation_frame, args.bin_width, 'corr_coef', max_mi)
-    region_to_info_hist = getRegionToHistDict(correlation_frame, args.bin_width, 'mutual_info', max_mi)
+    region_to_corr = getRegionToSampleDict(correlation_frame, args.bin_width, 'corr_coef')
+    region_to_info = getRegionToSampleDict(correlation_frame, args.bin_width, 'mutual_info')
     region_combinations = combinations(rc.regions, 2)
-    stats_p_values = np.array([getKSStatPVal(regions, region_to_corr_hist, region_to_info_hist) for regions in region_combinations])
+    stats_p_values = np.array([getKSStatPVal(regions, region_to_corr, region_to_info) for regions in region_combinations])
     stat_test_frame = pd.DataFrame(stats_p_values, columns=['first_region', 'second_region', 'corr_stat', 'corr_p_value', 'info_stat', 'info_p_value'])
     for col in ['corr_stat', 'corr_p_value', 'info_stat', 'info_p_value']:
         stat_test_frame.loc[:,col] = stat_test_frame.loc[:,col].astype(float)
     stat_test_frame.loc[:,'is_corr_sig'] = stat_test_frame.loc[:,'corr_p_value'] < 0.05
     stat_test_frame.loc[:,'is_info_sig'] = stat_test_frame.loc[:,'info_p_value'] < 0.05
+    print(stat_test_frame)
     filename = args.prefix + 'stats_tests.csv'
     stat_test_frame.to_csv(os.path.join(csv_dir, filename), index=False)
     print(dt.datetime.now().isoformat() + ' INFO: ' + filename + ' saved.')
