bcbb4889e8f51ae402fe7b651c4dfcd9e698335f
Thomas James Delaney <thomas.delaney@posteo.net>
2019-06-23 22:08:41 +0100

    Added a raster plotting function. Added a directory for poster images.

--- py/regionalCorrelationsPlotting.py
+++ py/regionalCorrelationsPlotting.py
@@ -15,3 +15,21 @@ def plotCorrMatrix(corr_matrix, cell_info, region_sorted_cell_ids):
     plt.setp(plt.xticks()[1], rotation=-45, ha="right", rotation_mode="anchor")
     plt.colorbar()
     plt.tight_layout()
+
+def plotRasterWithCellsAndTimes(region_sorted_cell_ids, spike_time_dict, start_time, stop_time, trials_info, cell_info):
+    num_cells = region_sorted_cell_ids.size
+    relevant_trials_info = trials_info[(start_time < trials_info[:,1]) & (trials_info[:,0] < stop_time),:]
+    relevant_cell_info = cell_info.loc[region_sorted_cell_ids]
+    for i, cell_id in enumerate(region_sorted_cell_ids):
+        spike_times = spike_time_dict[cell_id][(start_time < spike_time_dict[cell_id]) & (spike_time_dict[cell_id] < stop_time)]
+        if spike_times.size > 0:
+            plt.vlines(x=spike_times, ymin=i+0.05, ymax=i+0.95, color=region_to_colour[relevant_cell_info.loc[cell_id]['region']], alpha=1.0)
+    plt.ylim([0, num_cells])
+    for trial_info in relevant_trials_info:
+        trial_start = trial_info[0]; trial_stop = trial_info[1];
+        plt.fill_between(x=[trial_start, trial_stop], y1=0, y2=num_cells, color='black', alpha=0.2)
+    plt.xlabel('Time (s)', fontsize='large')
+    plt.xlim([start_time, stop_time])
+    plt.yticks(0.5+np.arange(num_cells), relevant_cell_info['region'].values, fontsize='large')
+    plt.tight_layout()
+
